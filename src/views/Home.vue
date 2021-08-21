60a41c9fc1acec1de12212224626f6843fea351b
yangseung-eun <ksook12@gmail.com>
2021-08-21 17:22:17 +0900

    vuejs Lifecycle study

---
+++ src/views/Home.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <h1>Welcome to {{title}}!</h1>
+        <input type="text" v-model="input1" />
+        <button type="button" @click="getData">Get</button>
+        <button type="button" @click="setData">Set</button>
+
+        <select class="form-control" v-model="region" @change="changeRegin">
+            <option :key="i" :value="d.v" v-for="(d,i) in options">{{d.t}}</option>
+        </select>
+
+        <table class="table table-bordered" v-show="tableShow">
+            <tr :key="i" v-for="(d,i) in options">
+                <td>{{d.v}}</td>
+                <td>{{d.t}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            title: "승은",
+            input1: "abc",
+            options:[
+                {v:"S", t:"Seoul"},
+                {v:"J", t:"Jeju"},
+                {v:"B", t:"Busan"},
+            ],
+            region:"J",
+            tableShow: true
+        }
+    },
+    watch: {
+        input1(){
+            console.log(this.input1);
+        },
+        title(){
+
+        }
+    },
+    methods:{
+        getData(){
+            alert(this.input1)
+        },
+        setData(){
+            this.input1="12345"
+        },
+        changeRegin(){
+            alert(this.region)
+        }
+    },
+    beforeCreate(){
+        console.log("beforeCreate")
+    },
+    created(){
+        console.log("created")
+    },
+    beforeMount(){
+        console.log("beforeMount")
+    },
+    mounted(){
+        console.log("mounted")
+    },
+    beforeUpdate(){
+        console.log("beforeUpdate")
+    },
+    updated(){
+        console.log("updated")
+    },
+    beforeDestroy(){
+        console.log("beforeDestroy")
+    },
+    destroyed(){
+        console.log("destroyed")
+    }
+};
+</script>
